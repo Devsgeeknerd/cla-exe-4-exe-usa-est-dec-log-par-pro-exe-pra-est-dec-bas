45741fed295a98ec3970a53a824796485314a5e1
Paulo <vanderley.1109@gmail.com>
2024-01-18 15:08:18 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,10 @@
+let tempoDeContribuicao: number = 35;
+let idade: number = 60;
+
+if (tempoDeContribuicao >= 35) {
+  console.log("Parabéns, você já pode entrar com a sua aposentadoria");
+} else if (tempoDeContribuicao >= 15 && idade >= 65) {
+  console.log("Parabéns, você já pode entrar com a sua aposentadoria");
+} else {
+  console.log("Você ainda não tem os requisitos mínimos para se aposentar");
+}
